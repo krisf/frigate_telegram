79a48e0d5acdec94fecf8f7501143fd62f67d046
Kris <YOUR@EMAIL.com>
2024-08-10 11:04:42 -0400

    dont shell out to curl

--- frigate.rb
+++ frigate.rb
@@ -12,30 +12,22 @@ mqtt_pass = ENV['MQTT_PASS']
 
 frigate_url = ENV['FRIGATE_URL']
 
+id_list = []
+
 
 Telegram::Bot::Client.run(token) do |bot|
   MQTT::Client.connect(host: mqtt_host, port: mqtt_port, username: mqtt_user, password: mqtt_pass) do |c|
     c.get('frigate/events') do |topic,message|
       a = JSON.parse message
-      if a['type'] == 'new' 
+      if a['type'] == 'update' && a['has_clip'] == true && !id_list.include?(a['before']['id'])
+        id_list << a['before']['id']
         formatted_message = "#{a['before']['camera'].capitalize} - #{a['before']['label'].capitalize} was detected."
         snapshot = "#{frigate_url}/api/events/#{a['before']['id']}/thumbnail.jpg"
-        #bot.api.send_message(chat_id: chat_id, text: formatted_message)
-        timeout_reached = 0.1
-        until `curl --write-out %{http_code} --silent -I --output /dev/null #{snapshot}` === "200" or timeout_reached > 10 do
-          sleep 0.1
-          timeout_reached = timeout_reached + 0.1
+        bot.api.send_message(chat_id: chat_id, text: formatted_message)
+        bot.api.send_photo(chat_id: chat_id, photo: snapshot, caption: formatted_message, show_caption_above_media: true, disable_notification: true)
         end
-        if timeout_reached < 10 && `curl --write-out %{http_code} --silent -I --output /dev/null #{snapshot}` === "200"
-          bot.api.send_photo(chat_id: chat_id, photo: snapshot, caption: formatted_message, show_caption_above_media: true, disable_notification: true)
-        end
-      elsif a['type'] == 'end'
+      elsif a['type'] == 'end' && a['has_clip'] == true
         clip = "#{frigate_url}/api/events/#{a['before']['id']}/clip.mp4"
-        timeout_reached = 0.1
-        until `curl --write-out %{http_code} --silent -I --output /dev/null #{clip}` === "200" or timeout_reached > 10 do
-          sleep 0.1
-          timeout_reached = timeout_reached + 0.1
-        end
         bot.api.send_video(chat_id: chat_id, video: clip, caption: formatted_message, show_caption_above_media: true, supports_streaming: true, disable_notification: true)
       else
         puts "skipped message, not new"
